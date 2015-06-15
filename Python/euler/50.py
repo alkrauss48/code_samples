5688d7afa1107150f1b4e28355fda46254d205cc
aaron <alkrauss48@gmail.com>
2015-06-15 10:20:26 -0500

    Object-orientify problem 50

--- Python/euler/50.py
+++ Python/euler/50.py
@@ -1,33 +1,57 @@
+# The prime 41, can be written as the sum of six consecutive primes:
+#
+# 41 = 2 + 3 + 5 + 7 + 11 + 13
+# This is the longest sum of consecutive primes that adds to a prime below one-hundred.
+#
+# The longest sum of consecutive primes below one-thousand that adds to a prime, contains 21 terms, and is equal to 953.
+#
+# Which prime, below one-million, can be written as the sum of the most consecutive primes?
+
 from prime_generator import get_primes
+
+class LongestSum:
+    primes      = get_primes(1000000)
+    max_value   = 1000000
+
+    def __init__(self, index):
+        self.index          = index
+        self.current_sum    = 0
+        self.largest_sum    = 0
+        self.count          = 0
+        self.largest_counts = []
+
+    def determine_sum(self):
+        added = None
+        while self.current_sum < 1000000:
+            self.current_sum += LongestSum.primes[self.index]
+            self.count += 1
+            if self.current_sum in LongestSum.primes:
+                self.largest_sum = self.current_sum
+                self.largest_counts.append(self.count)
+                added = True
+            else:
+                added = False
+            self.index += 1
+
+        # Because python doesn't support a do-while loop, this loop will run 1
+        # loop over the maximum value. If during that last loop, the resulting
+        # current_sum is in fact a prime number, we need to remove it because
+        # it's an invalid answer.
+        if added: _remove_last_entries(self)
+
+    def largest_count(self):
+        return max(self.largest_counts)
+
+    def _remove_last_entries(self):
+        self.largest_sum -= LongestSum.primes[self.index - 1]
+        self.largest_count.pop()
+
 
 # Had to guess and check 3. I assumed 0 would produce the longest sum, since it
 # would include the lowest valued prime numbers. I was wrong.
 index = 3
+l = LongestSum(index)
+l.determine_sum()
 
-primes = get_primes(1000000)
-
-current_sum = 0
-largest_sum = 0
-added = None
-count = 0
-string = "0"
-
-while current_sum < 1000000:
-    current_sum += primes[index]
-    string += " + {0}".format(current_sum)
-    count += 1
-    if current_sum in primes:
-        largest_sum = current_sum
-        largest_count = count
-        added = True
-    else:
-        added = False
-    index += 1
-
-if added:
-    largest_sum     -= primes[index - 1]
-    largest_count   -= 1
-
-print(largest_sum)
-print(largest_count)
-print(string)
+print(l.largest_sum)
+print(l.largest_count())
